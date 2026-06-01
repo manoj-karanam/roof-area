bc131e307dc30942c427ee660edb3fb9002ef819
manojpfw <151407816+manojkaranamm-stack@users.noreply.github.com>
2026-05-31 21:34:27 -0400

    Add: user can enter address and solar API data is fetched

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,19 @@ from fastapi import FastAPI
 from services.geocode import get_coordinates
 from services.solar import get_roof_data
 import logging, os  
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI()
+
+# Handling CORS issue
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -18,38 +30,30 @@ def root():
 
 
 @app.get("/analyze")
-def analyze(address: str):
+def analyze(lat: float, lng: float, address: str = None):
+    
+    if address is None or lat is None or lng is None:
+        logger.error("Missing required parameters")
+        return {"error": "address, lat, lng are required"}
 
-    logger.info(f"Received address: {address}")
+    logger.info( f"Address: {address}")
+    logger.info(f"Received Successfully: Latitude={lat}, Longitude={lng}")
 
-    # STEP 1: Geocoding
-    logger.info("Step 1: Converting address to coordinates using Geocoding API")
-    coords = get_coordinates(address)
+    logger.info("Sending coordinates to Solar API")
+    logger.info(f"Requesting roof data for coordinates: {lat}, {lng}")
 
-    if not coords:
-        logger.error("Geocoding failed - no coordinates returned")
-        return {"error": "Invalid address or geocoding failed"}
-
-    logger.info(
-        f"Geocoding successful: lat={coords['lat']}, lng={coords['lng']}"
-    )
-
-    # STEP 2: Solar API
-    logger.info("Step 2: Sending coordinates to Solar API")
-    logger.info(f"Requesting roof data for: {coords}")
-
-    roof = get_roof_data(coords["lat"], coords["lng"])
+    roof = get_roof_data(lat, lng)
 
     if not roof:
         logger.error("Solar API failed or returned empty response")
         return {"error": "Solar API failed"}
 
     logger.info("Solar API response received successfully")
-
-    # STEP 3: Response
     logger.info("Returning roof + coordinate data to frontend")
 
     return {
-        "coordinates": coords,
+        "address": address,
+        "latitude": lat,
+        "longitude": lng,
         "roof_data": roof
     }
